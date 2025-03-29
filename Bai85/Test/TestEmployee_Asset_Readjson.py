b63dab57edfdde1589eb947845b2a880b288fd8d
QuangMinhJQK <nvqm2006@gmail.com>
2025-03-29 11:50:25 +0700

    Delete Bai85 directory

--- Bai85/Test/TestEmployee_Asset_Readjson.py
+++
@@ -1,9 +0,0 @@
-from Baitap.Bai85.Libs.JsonFileFactory import JsonFileFactory
-from Baitap.Bai85.Models.Employee_Asset import Employee_Asset
-
-jff = JsonFileFactory()
-filename = "../DataSet/employees_assets.json"
-employees_assets = jff.read_data(filename,Employee_Asset)
-print("danh sach Asset sau khi doc file")
-for ea in employees_assets:
-    print(ea)
